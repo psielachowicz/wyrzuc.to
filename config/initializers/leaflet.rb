22306a0542542c1c99857b505b02604c0e4ef232
psielachowicz <jaguar6@wp.pl>
2014-12-02 18:55:03 +0100

    pierwszy szkic
    PIerwszy zarys strony bez funkcjonalnosci, przwidywany commit z
    pierwszymi funkcjonalnosciami jutro przed 13

---
+++ config/initializers/leaflet.rb
@@ -0,0 +1,3 @@
+Leaflet.tile_layer = 'http://b.tile.openstreetmap.org/8/139/85.png'
+Leaflet.attribution = '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+Leaflet.max_zoom = 18
